a3814d4d112461bedd78cd20a71943a2de45929a
GregoireF <gfavreau.wrprojects@gmail.com>
2022-12-28 14:06:19 +0100

    📦build(dist): build with tsc

---
+++ packages/commitizen-config/dist/constant.js
@@ -0,0 +1,320 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.types = void 0;
+exports.types = {
+    init: {
+        description: "Begin a project.",
+        title: "Initial Commit",
+        emoji: "🎉",
+    },
+    tag: {
+        description: "Creation of a version number",
+        title: "Creation of version",
+        emoji: "🔖",
+    },
+    feat: {
+        description: "Introduce new features.",
+        title: "Features",
+        emoji: "✨",
+    },
+    bug: {
+        description: "Fix a bug.",
+        title: "Bug",
+        emoji: "🐛",
+    },
+    breaking: {
+        description: "",
+        title: "",
+        emoji: "💥",
+    },
+    critical: {
+        description: "Critical hotfix.",
+        title: "Hotfix",
+        emoji: "🚑",
+    },
+    fix: {
+        description: "A bug fix",
+        title: "Bug Fixes",
+        emoji: "🩹",
+    },
+    wip: {
+        description: "Work In Progress",
+        title: "Work In Progress",
+        emoji: "🚧",
+    },
+    comment: {
+        description: "Adding comments to the code",
+        title: "Comment",
+        emoji: "📝",
+    },
+    permissions: {
+        description: "Add/Remove/Modify Permissions",
+        title: "Permissions",
+        emoji: "🛂",
+    },
+    roles: {
+        description: "Add/Remove/Modify Roles",
+        title: "Rôles",
+        emoji: "🛂",
+    },
+    security: {
+        description: "Security Code",
+        title: "Security",
+        emoji: "🔐",
+    },
+    secrets: {
+        description: "Add/Remove/Modify Secrets",
+        title: "Secrets",
+        emoji: "🔐",
+    },
+    design: {
+        description: "Design Code",
+        title: "Design",
+        emoji: "💄",
+    },
+    animations: {
+        description: "Add/Remove/Modify animations",
+        title: "Animations",
+        emoji: "🎨",
+    },
+    transitions: {
+        description: "Adding/Deleting/Modifying transitions",
+        title: "Transitions",
+        emoji: "🎨",
+    },
+    improve: {
+        description: "Code improvement",
+        title: "Improve",
+        emoji: "💎",
+    },
+    perf: {
+        description: "Code performance",
+        title: "Performance",
+        emoji: "🐎",
+    },
+    refactor: {
+        description: "Refactoring code",
+        title: "Refactor",
+        emoji: "♻️",
+    },
+    accessibility: {
+        description: "Code accessibility",
+        title: "accessibility",
+        emoji: "♿",
+    },
+    desktop: {
+        description: "Design desktop",
+        title: "Desktop",
+        emoji: "💻",
+    },
+    mobile: {
+        description: "Design mobile",
+        title: "Mobile",
+        emoji: "📱",
+    },
+    responsive: {
+        description: "Design responsive",
+        title: "Responsive",
+        emoji: "📱💻",
+    },
+    windows: {
+        description: "Windows OS",
+        title: "Windows OS",
+        emoji: "🏁",
+    },
+    macOS: {
+        description: "MacOS",
+        title: "Mac OS",
+        emoji: "🍎",
+    },
+    linux: {
+        description: "Linux OS",
+        title: "Linux OS",
+        emoji: "🐧",
+    },
+    test: {
+        description: "Code related to tests",
+        title: "Test",
+        emoji: "🧪",
+    },
+    addingtest: {
+        description: "Adding tests",
+        title: "Adding test",
+        emoji: "➕",
+    },
+    build: {
+        description: "Build your project",
+        title: "Build",
+        emoji: "📦",
+    },
+    deploy: {
+        description: "Deploy in production",
+        title: "Deploy",
+        emoji: "🚀",
+    },
+    ci: {
+        description: "Add/Delete/Modify CIs",
+        title: "CI",
+        emoji: "👷",
+    },
+    devscripts: {
+        description: "Add/Delete/Modify scripts for devs",
+        title: "Dev Scripts",
+        emoji: "📜",
+    },
+    docs: {
+        description: "Add/Delete/Modify Documentation",
+        title: "Docs",
+        emoji: "📚",
+    },
+    internationalize: {
+        description: "Add/Remove/Modify supported languages",
+        title: "Internationalize",
+        emoji: "🌐",
+    },
+    i18n: {
+        description: "Code related to i18n",
+        title: "I18N",
+        emoji: "🌐",
+    },
+    localisation: {
+        description: "Code related to the location",
+        title: "Localisation",
+        emoji: "🌐",
+    },
+    metadata: {
+        description: "Add/Delete/Modify metadata",
+        title: "MetaData",
+        emoji: "📊",
+    },
+    lint: {
+        description: "Action of linting a file(s)",
+        title: "Lint",
+        emoji: "👕",
+    },
+    architectural: {
+        description: "Modification of the project's architecture",
+        title: "Architectural",
+        emoji: "🏗️",
+    },
+    generalupdate: {
+        description: "General change",
+        title: "General Update",
+        emoji: "⚡",
+    },
+    upgrade: {
+        description: "Upgrade dependencies.",
+        title: "Upgrade",
+        emoji: "⬆️",
+    },
+    downgrade: {
+        description: "Downgrade dependencies.",
+        title: "Downgrade",
+        emoji: "⬇️",
+    },
+    add: {
+        description: "Add a file/folder",
+        title: "Add",
+        emoji: "➕",
+    },
+    remove: {
+        description: "Remove code or files.",
+        title: "Remove",
+        emoji: "🔥",
+    },
+    move: {
+        description: "Move a file/folder",
+        title: "Move",
+        emoji: "🚚",
+    },
+    rename: {
+        description: "Rename a file/folder",
+        title: "Rename",
+        emoji: "🚚",
+    },
+    adddep: {
+        description: "Adding dependencies",
+        title: "Add Dependencies",
+        emoji: "➕",
+    },
+    removedep: {
+        description: "Deleting dependencies",
+        title: "Remove Dependences",
+        emoji: "➖",
+    },
+    contributor: {
+        description: "Add/Delete/Modify contributors",
+        title: "Contributors",
+        emoji: "👥",
+    },
+    docker: {
+        description: "Docker related code",
+        title: "Docker",
+        emoji: "🐳",
+    },
+    configfile: {
+        description: "Add/Delete/Modify Config Files",
+        title: "Config File",
+        emoji: "🔧",
+    },
+    api: {
+        description: "Code related to the API",
+        title: "API",
+        emoji: "👽",
+    },
+    license: {
+        description: "Add/Delete/Modify licenses",
+        title: "License",
+        emoji: "📄",
+    },
+    assets: {
+        description: "Add/Remove/Modify assets (img,fonts,...)",
+        title: "Assets",
+        emoji: "🍱",
+    },
+    gitfile: {
+        description: "Add/Modify gitfile",
+        title: "GitFile",
+        emoji: "✏️",
+    },
+    snapshots: {
+        description: "Add/Delete/Modify snapshots",
+        title: "Snapshots",
+        emoji: "📸",
+    },
+    experiments: {
+        description: "Experimentation in the code",
+        title: "Experiments",
+        emoji: "⚗️",
+    },
+    seo: {
+        description: "Code related to SEO",
+        title: "SEO",
+        emoji: "🔍️",
+    },
+    database: {
+        description: "Code related to the database",
+        title: "Database",
+        emoji: "🗃️",
+    },
+    merge: {
+        description: "Perform the update",
+        title: "Merge",
+        emoji: "🔀",
+    },
+    needforks: {
+        description: "Need a fork",
+        title: "Need forks",
+        emoji: "🔄",
+    },
+    dead: {
+        description: "Dead code",
+        title: "Dead",
+        emoji: "⚰️",
+    },
+    bad: {
+        description: "Bad code",
+        title: "Bad",
+        emoji: "🗑️",
+    },
+};
