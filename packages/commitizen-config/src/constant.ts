f76f2c683a1e05a6e2449c0d86c4baca47505ca9
GregoireF <gfavreau.wrprojects@gmail.com>
2022-12-28 01:31:04 +0100

    major update

---
+++ packages/commitizen-config/src/constant.ts
@@ -0,0 +1,342 @@
+export const types = {
+    init: {
+      description: "Begin a project.",
+      title: "Initial Commit",
+      emoji: "🎉",
+    },
+    tag: {
+      description: "",
+      title: "",
+      emoji: "🔖",
+    },
+    feat: {
+      description: "Introduce new features.",
+      title: "Features",
+      emoji: "✨",
+    },
+    bug: {
+      description: "Fix a bug.",
+      title: "Bug",
+      emoji: "🐛",
+    },
+    breaking: {
+      description: "",
+      title: "",
+      emoji: "💥",
+    },
+    critical: {
+      description: "Critical hotfix.",
+      title: "Hotfix",
+      emoji: "🚑",
+    },
+    fix: {
+      description: "A bug fix",
+      title: "Bug Fixes",
+      emoji: "🩹",
+    },
+    wip: {
+      description: "",
+      title: "",
+      emoji: "🚧",
+    },
+    comment: {
+      description: "",
+      title: "",
+      emoji: "📝",
+    },
+    permissions: {
+      description: "",
+      title: "",
+      emoji: "🛂",
+    },
+    roles: {
+      description: "",
+      title: "",
+      emoji: "🛂",
+    },
+    security: {
+      description: "",
+      title: "",
+      emoji: "🔐",
+    },
+    secrets: {
+      description: "",
+      title: "",
+      emoji: "🔐",
+    },
+    design: {
+      description: "",
+      title: "",
+      emoji: "💄",
+    },
+    style: {
+      description: "",
+      title: "",
+      emoji: "💄",
+    },
+    animations: {
+      description: "",
+      title: "",
+      emoji: "🎨",
+    },
+    transitions: {
+      description: "",
+      title: "",
+      emoji: "🎨",
+    },
+    improve: {
+      description: "",
+      title: "",
+      emoji: "💎",
+    },
+    perf: {
+      description: "",
+      title: "",
+      emoji: "🐎",
+    },
+    refactor: {
+      description: "",
+      title: "",
+      emoji: "♻️",
+    },
+    accessability: {
+      description: "",
+      title: "",
+      emoji: "♿",
+    },
+    desktop: {
+      description: "",
+      title: "",
+      emoji: "💻",
+    },
+    mobile: {
+      description: "",
+      title: "",
+      emoji: "📱",
+    },
+    responsive: {
+      description: "",
+      title: "",
+      emoji: "📱💻",
+    },
+    windows: {
+      description: "",
+      title: "Windows OS",
+      emoji: "🏁",
+    },
+    macOS: {
+      description: "",
+      title: "Mac OS",
+      emoji: "🍎",
+    },
+    linux: {
+      description: "Linux OS",
+      title: "",
+      emoji: "🐧",
+    },
+    test: {
+      description: "",
+      title: "",
+      emoji: "➕",
+    },
+    addingtest: {
+      description: "",
+      title: "",
+      emoji: "➕",
+    },
+    build: {
+      description: "",
+      title: "",
+      emoji: "📦",
+    },
+    deploy: {
+      description: "",
+      title: "",
+      emoji: "🚀",
+    },
+    ci: {
+      description: "",
+      title: "",
+      emoji: "👷",
+    },
+    updateci: {
+      description: "",
+      title: "",
+      emoji: "👷",
+    },
+    devscripts: {
+      description: "",
+      title: "",
+      emoji: "📜",
+    },
+    docs: {
+      description: "",
+      title: "",
+      emoji: "📚",
+    },
+    internationalize: {
+      description: "",
+      title: "",
+      emoji: "🌐",
+    },
+    i18n: {
+      description: "",
+      title: "",
+      emoji: "🌐",
+    },
+    localisation: {
+      description: "",
+      title: "",
+      emoji: "🌐",
+    },
+    metadata: {
+      description: "",
+      title: "",
+      emoji: "📊",
+    },
+    lint: {
+      description: "",
+      title: "",
+      emoji: "👕",
+    },
+    architectural: {
+      description: "",
+      title: "",
+      emoji: "🏗️",
+    },
+    generalupdate: {
+      description: "",
+      title: "",
+      emoji: "⚡",
+    },
+    upgrade: {
+      description: "Upgrade dependencies.",
+      title: "Upgrade",
+      emoji: "⬆️",
+    },
+    downgrade: {
+      description: "Downgrade dependencies.",
+      title: "Downgrade",
+      emoji: "⬇️",
+    },
+    add: {
+      description: "",
+      title: "",
+      emoji: "➕",
+    },
+    remove: {
+      description: "Remove code or files.",
+      title: "Remove",
+      emoji: "🔥",
+    },
+    move: {
+      description: "",
+      title: "",
+      emoji: "🚚",
+    },
+    rename: {
+      description: "",
+      title: "",
+      emoji: "🚚",
+    },
+    adddep: {
+      description: "",
+      title: "",
+      emoji: "➕",
+    },
+    removedep: {
+      description: "",
+      title: "",
+      emoji: "➖",
+    },
+    pindep: {
+      description: "",
+      title: "",
+      emoji: "📌",
+    },
+    contributor: {
+      description: "",
+      title: "",
+      emoji: "👥",
+    },
+    docker: {
+      description: "",
+      title: "",
+      emoji: "🐳",
+    },
+    configfile: {
+      description: "",
+      title: "",
+      emoji: "🔧",
+    },
+    api: {
+      description: "",
+      title: "",
+      emoji: "👽",
+    },
+    license: {
+      description: "",
+      title: "",
+      emoji: "📄",
+    },
+    assets: {
+      description: "",
+      title: "",
+      emoji: "🍱",
+    },
+    gitfile: {
+      description: "",
+      title: "",
+      emoji: "✏️",
+    },
+    snapshots: {
+      description: "",
+      title: "",
+      emoji: "📸",
+    },
+    experiments: {
+      description: "",
+      title: "",
+      emoji: "⚗️",
+    },
+    seo: {
+      description: "",
+      title: "",
+      emoji: "🔍️",
+    },
+    database: {
+      description: "",
+      title: "",
+      emoji: "🗃️",
+    },
+    merge: {
+      description: "",
+      title: "",
+      emoji: "🔀",
+    },
+    needforks: {
+      description: "",
+      title: "",
+      emoji: "🔄",
+    },
+    revert: {
+      description: "",
+      title: "",
+      emoji: "⏪",
+    },
+    dead: {
+      description: "",
+      title: "",
+      emoji: "⚰️",
+    },
+    bad: {
+      description: "",
+      title: "",
+      emoji: "🗑️",
+    },
+    okcode: {
+      description: "",
+      title: "",
+      emoji: "👌",
+    },
+}
